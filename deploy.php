20a262fbd830ef7656fa28c713cc9338a165b530
Anton <aner-anton@yandex.ru>
2023-09-11 17:46:41 +0300

    deployer

---
+++ deploy.php
@@ -0,0 +1,31 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Config
+
+set('repository', 'https://github.com/AntonLeontev/um-landing.git');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Tasks
+
+task('build', function () {
+    cd('{{release_path}}');
+    run('npm install');
+    run('npm run build');
+});
+
+// Hosts
+
+host('195.93.253.43')
+    ->set('remote_user', 'deployer')
+    ->set('deploy_path', '~/umLanding');
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
+after('deploy:vendors', 'build');
