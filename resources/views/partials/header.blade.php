47c72d76c21f87ae88ebfc27c5958fe6591d92d4
Anton <aner-anton@yandex.ru>
2023-09-22 13:55:17 +0300

    add: header username

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,12 +5,13 @@
 	@auth
 		<div class="flex-none">
 			<div class="dropdown dropdown-end">
-				<label tabindex="0" class="btn btn-ghost btn-circle avatar">
-					<div class="w-10 rounded-full">
+				<label tabindex="0" class="btn btn-ghost avatar">
+					<div class="">
 						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
 							<path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
 						</svg>
 					</div>
+					<span>{{ auth()->user()->name }}</span>
 				</label>
 				<ul tabindex="0" class="z-20 p-2 mt-3 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
 					<li><a>Profile</a></li>
