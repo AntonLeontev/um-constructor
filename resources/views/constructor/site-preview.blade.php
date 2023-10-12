4c8db70e01b46ac8d73bdde292e9446e1f085608
Anton <aner-anton@yandex.ru>
2023-10-12 18:44:19 +0300

    notice for blank site

--- resources/views/constructor/site-preview.blade.php
+++ resources/views/constructor/site-preview.blade.php
@@ -3,7 +3,21 @@
 @section('title', 'Title')
 
 @section('content')
-	@foreach ($site->blocks->load('stringData') as $block) 
-		{!! $block->class->view($block->getSavedData()) !!}
-	@endforeach
+	@if ($site->blocks->isEmpty())
+		<style>
+			.notice {
+				height: 100vh;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+			}
+		</style>
+		<div class="notice">
+			This site is blank. You can build it in the constructor
+		</div>
+	@else
+		@foreach ($site->blocks->load('stringData') as $block) 
+			{!! $block->class->view($block->getSavedData()) !!}
+		@endforeach
+	@endif
 @endsection
