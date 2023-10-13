07321d7bae076ec027d13c9ef5ea9acfd2743015
Anton <aner-anton@yandex.ru>
2023-10-13 11:07:52 +0300

    delay for toasts

--- resources/views/partials/toasts.blade.php
+++ resources/views/partials/toasts.blade.php
@@ -3,6 +3,7 @@
 	x-data="toasts" 
 	@toast.window="add"
 	@toast-error.window="addError"
+	@toast-success.window="addSuccess"
 >
 	<template x-for="toast in toasts">
 		<div class="transition alert" :class="typeObject(toast.type)" @click="remove(toast.id)">
@@ -23,25 +24,44 @@
 					'alert-success': type === 'success',
 				}
 			},
+			push(mesage, type, delay = 0) {
+				this.toasts.push({
+					id: this.toasts.length + 1,
+					message: mesage,
+					type: type,
+				})
+
+				if (delay > 0) {
+					setTimeout(() => {
+						this.remove(this.toasts.length + 1);
+					}, delay);
+				}
+			},
 			remove(id) {
 				let toast = this.toasts.find(el => el.id === id);
 				let index = this.toasts.indexOf(toast);
 				this.toasts.splice(index, 1);
 			},
 			add() {
-				this.toasts.push({
-					id: this.toasts.length + 1,
-					message: this.$event.detail.message,
-					type: this.$event.detail.type,
-				})
+				this.push(
+					this.$event.detail.message, 
+					this.$event.detail.type,
+					this.$event.detail.delay
+				)
 			},
 			addError() {
-				this.toasts.push({
-					id: this.toasts.length + 1,
-					message: this.$event.detail,
-					type: 'error',
-				})
+				this.push(
+					this.$event.detail, 
+					'error',
+				)
 			},
+			addSuccess() {
+				this.push(
+					this.$event.detail.message, 
+					'success',
+					this.$event.detail.delay, 
+				)
+			}
 
 		}))
 	})
