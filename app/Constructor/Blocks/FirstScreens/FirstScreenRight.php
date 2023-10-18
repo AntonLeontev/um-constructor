dea2713ebd4df5db5fbdf2be6dd23c4a66352dcd
Anton <aner-anton@yandex.ru>
2023-10-18 18:06:37 +0300

    colors in FirstScreenRight

--- app/Constructor/Blocks/FirstScreens/FirstScreenRight.php
+++ app/Constructor/Blocks/FirstScreens/FirstScreenRight.php
@@ -36,6 +36,18 @@ class FirstScreenRight extends AbstractBlock
                 'width' => '400',
                 'height' => '600',
             ],
+            'background_color' => [
+                'value' => '',
+                'type' => DataType::color,
+                'label' => 'Background color',
+                'css_property' => 'background-color',
+            ],
+            'font_color' => [
+                'value' => '',
+                'type' => DataType::color,
+                'label' => 'Font color',
+                'css_property' => 'color',
+            ],
         ];
     }
 
