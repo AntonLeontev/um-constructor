65e545b1c451499421a18cb75ba5c06ba6b7d7ad
Anton <aner-anton@yandex.ru>
2023-12-12 13:42:17 +0300

    fix login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,13 +9,13 @@
 				<div class="text-center">{{ __('auth.login to your account') }}</div>
 				@csrf
 				<div class="">
-					<input type="text" class="input @error('email') input-error @else input-primary @enderror" name="email" placeholder="Email">
+					<input type="text" class="input @error('email') input-error @else input-primary @enderror w-full" name="email" placeholder="Email">
 					@error('email')
 						<div class="text-error">{{ $errors->first('email') }}</div>
 					@enderror
 				</div>
 				<div class="">
-					<input type="password" class="input @error('password') input-error @else input-primary @enderror" name="password" placeholder="{{ __('auth.password') }}">
+					<input type="password" class="input @error('password') input-error @else input-primary @enderror w-full" name="password" placeholder="{{ __('auth.password') }}">
 					@error('password')
 						<div class="text-error">{{ $errors->first('password') }}</div>
 					@enderror
